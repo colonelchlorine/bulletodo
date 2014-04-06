8ad52efae7e2cc7871f927efad17c17741f9a7f8
colonelchlorine <mikemurko@gmail.com>
2014-04-06 13:21:00 -0700

    Now saves your created bullets and their properties to localStorage.

--- todo.js
+++ todo.js
@@ -1,15 +1,38 @@
-angular.module("myApp", []).controller("TreeController", ['$scope', function($scope) {
+angular.module("myApp", []).service('uuid4', function() {
+  /**! http://stackoverflow.com/a/2117523/377392 */
+  var fmt = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx';
+  this.generate = function() {
+    return fmt.replace(/[xy]/g, function(c) {
+      var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
+      return v.toString(16);
+    });
+  };
+}).controller("TreeController", ['$scope', 'uuid4', function($scope, uuid4) {
+    $scope.tree = [];
+    
+    $scope.init = function () {
+        $scope.tree = JSON.parse(window.localStorage.getItem("bullets"));
+
+        if ($scope.tree === null) {
+            $scope.tree = [];
+        }
+    }
+
     $scope.delete = function(data) {
         data.nodes = [];
+        $scope.save();
     };
+
     $scope.add = function(data) {
         var post = data.nodes.length + 1;
         var newName = data.name + '-' + post;
-        data.nodes.push({name: newName, nodes: [], done:false, edit: false, nodeHide: false});
+        data.nodes.push({name: newName, nodes: [], done:false, edit: false, hide: false, guid: uuid4.generate()});
+        $scope.save();
     };
 
     $scope.flipEdit = function (data) {
         data.edit = !data.edit;
+        $scope.save();
     };
 
     $scope.editSave = function (data, $event, isBlurred) {
@@ -18,16 +41,19 @@ angular.module("myApp", []).controller("TreeController", ['$scope', function($sc
             data.name = $event.target.value;
             this.flipEdit(data);
         }
+        $scope.save();
     };
 
-    $scope.nodeHide = function (data, hideIt) {
-        data.nodeHide = hideIt;
+    $scope.hide = function (data, hideIt) {
+        data.hide = hideIt;
+        $scope.save();
     }
 
     $scope.addTodo = function(tree, $event) {
         //Enter
         if ($event.which == 13) {
-            tree.push({name:$event.target.value, nodes: [], done:false, edit: false, nodeHide: false});
+            tree.push({name:$event.target.value, nodes: [], done:false, edit: false, hide: false, guid: uuid4.generate()});
+            $scope.save();
             $event.target.value = "";
 
             $event.preventDefault();
@@ -44,5 +70,8 @@ angular.module("myApp", []).controller("TreeController", ['$scope', function($sc
         }
     };
 
-    $scope.tree = [];
+    $scope.save = function () {
+        window.localStorage.setItem("bullets", JSON.stringify($scope.tree));
+    }
 }]);
+
